28440cad078ffc5cfeaf077115c4afec43753c6f
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-03-28 21:24:26 -0700

    .

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 const { Item } = require('../../models');
 
+router.get('', async (req, res) => {
+
+    
+})
+
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', async (req, res) => {
+
+    const userData = await User.findAll([
+        attributes: { exclude: "password" }
+    ])
+})
+
 
 module.exports = router;
