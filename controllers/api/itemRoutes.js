13e0e46d89558684278a4a77c2699c9e520586b6
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-03-29 20:14:46 -0700

    merge

--- controllers/api/itemRoutes.js
+++ controllers/api/itemRoutes.js
@@ -1,9 +1,38 @@
 const router = require('express').Router();
 const { Item } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('', async (req, res) => {
-
-    
-})
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newProject = await Project.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const projectData = await Project.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!projectData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(projectData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });)
 
 module.exports = router;
