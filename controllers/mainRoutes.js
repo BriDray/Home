255583d60c19d6a5b2ecc8c2fa8c7cc4251bb24d
Jonah Lewis <jonahslewis96@gmail.com>
2023-04-01 20:37:14 -0700

    added helpers and added to main.handlebars

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -2,6 +2,25 @@ const router = require('express').Router();
 const { Item, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // get all items
 router.get('/', async (req, res) => {
@@ -49,25 +68,6 @@ router.get('/item/:id', async (req, res) => {
     }
   });
 
-  // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const session = require('express-session');
-const expressHbs = require('express-handlebars');
+const exphbs  = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 // const database = require("./db/db.json");
 // const { v4: uuidv4 } = require('uuid');
 
@@ -11,6 +12,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 
 // login session
 const sess = {
@@ -32,8 +37,8 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js which template engine we're using
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
