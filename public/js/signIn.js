f7ba6edfde041ba3fac2c41b4f3a2414e7778b16
Jonah Lewis <jonahslewis96@gmail.com>
2023-04-03 19:29:28 -0700

    login session and functions are working

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,30 +1,38 @@
-var signInButton = document.querySelector('#sign-in');
+var signInButton = document.getElementById('signInButton');
 
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+const loginFormHandler = async () => {
+  // event.preventDefault();
 
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
+  const email = document.querySelector('#e-mail').value.trim();
+  const password = document.querySelector('#Password').value.trim();
 
   if (email && password) {
-    const response = await fetch('/api/users/login', {
+    const response = await fetch('/api/users/signin', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      // document.location.replace('/');
+      console.log("signed in")
     } else {
       alert('Failed to log in');
     }
   }
 };
-
-
-
+ console.log("running");
 
 // event listener for sign in button to redirect to main page
-signInButton.addEventListener("click", function(){
-  document.location.href = '/';
+// signInButton.addEventListener("click", function(){
+//   console.log("click")
+//   loginFormHandler();
+//   document.location.href = '/';
+//   console.log("")
+// });
+
+signInButton.addEventListener('click', function(event){
+  event.preventDefault();
+  loginFormHandler();
+  
 });
