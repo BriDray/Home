ba555e117c3039d4745d5b4244d4e012553a6cc8
Jonah Lewis <jonahslewis96@gmail.com>
2023-04-03 21:35:15 -0700

    SignUpPage GET is working

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -83,4 +83,14 @@ router.get('/item/:id', async (req, res) => {
     }
   });
 
-
+  router.get('/SignUpPage', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('SignUpPage');
+  });
+  
+  module.exports = router;
