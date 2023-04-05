edb11d555971488594904bf4f721b0bb3c503e28
Jonah Lewis <jonahslewis96@gmail.com>
2023-04-05 15:41:10 -0700

    create user function and route fixed and working

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,13 +5,14 @@ const signupFormHandler = async (event) => {
   
     const firstName = document.querySelector('#first_name').value.trim();
     const lastName = document.querySelector('#last_name').value.trim();
+    const name = firstName + (" ") + lastName;
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
   
-    if (firstName && lastName && email && password) {
-      const response = await fetch('/api/users/signUpPage', {
+    if (name && email && password) {
+      const response = await fetch('/api/users/signup', {
         method: 'POST',
-        body: JSON.stringify({ firstName, lastName, email, password }),
+        body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
